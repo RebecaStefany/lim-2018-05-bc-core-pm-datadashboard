a9fb682997eeb34d4e21a975d851bd50847b1416
Miluh <maytacasas21@gmail.com>
2018-06-21 18:52:21 -0500

    pinto usuarios

--- src/data.js
+++ src/data.js
@@ -1,24 +1,72 @@
-window.data = {
-  function1: function (cohorts) {
-    var htmlString = ''
-    for (i = 0; i < cohorts.length; i++) {
-      //if(cohorts[i].id === "lim-2018-03-pre-core-pw") {
-        htmlString += '<option value =' + i + '>' + cohorts[i].id + '.</option>';
-      //}
-    }
-  return htmlString;
+/*window.data = {
+  computeUsersStasts: () => {
+    
   },
-  function2: function (users) {
-    var htmlString1 = '' 
-    for (i = 0; i < users.length; i++) {
-      htmlString1 += '<option value =' + i + '>' + users[i].name + '.</option>';
-    }
-   return htmlString1;    
+  sortUsers: () => {
+
   },
+  filterUsers: () => {
+
+  },
+  processCohortData: () => {
+    
+  } 
+}*/
 
 
-}
+url1 = 'http://127.0.0.1:8080/data/cohorts.json'
+url2 = 'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json'
+url3 = 'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json'
+
+let selectCohorts = document.getElementById('select_cohorts');
+let filterButton = document.getElementById('filter_button');
+let usersList = document.getElementById('users_list');
+//let usersButton = document.getElementById('users_button')
+
+fetch(url1)
+ .then((response) => {
+   return response.json();
+ })
+ .then((cohorts) => {
+   let paintCohorts = '';
+   for (i = 0; i < cohorts.length; i++) {
+     paintCohorts += '<option value="' + i + '">' + cohorts[i].id + '</option>';
+   }
+   selectCohorts.innerHTML = paintCohorts;
+ })
+
+filterButton.addEventListener("click", function () {
+  let value = selectCohorts.options[selectCohorts.selectedIndex].text
+  if(value === 'lim-2018-03-pre-core-pw') { 
+    fetch(url3)
+    .then((response) => {
+      return response.json();
+    })
+    .then((users) => {
+      const filterRoles = users.filter(user => (user.role === "student"));
+      let paintUsers = '';
+      for (i = 0; i < filterRoles.length; i++) {
+        let roleStudents = filterRoles.map (filterRole => `${filterRole.name}`);
+        paintUsers += '<div>' + roleStudents[i] + '</div>';
+      }
+    usersList.innerHTML = paintUsers
+    })   
+  } else {
+  console.log("No hay data")}
+}) 
 
 
 
-     
+usersList.style.cursor = 'pointer';
+usersList.onclick = function() {
+  console.log("Hello")
+};
+
+/*const button = document.createElement("button");
+
+const content = document.createTextNode("Ver");
+
+button.appendChild(content);
+
+document.body.appendChild(button);*/
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-var enterButton = document.getElementById('enter_button');
-var platformButton = document.getElementById('platform_Button');
+var loginButton = document.getElementById('login_button');
+var studentsButton = document.getElementById('students_button');
+
+var view1 = document.getElementById('view_1');
+var view2 = document.getElementById('view_2');
+var view3 = document.getElementById('view_3');
+
+loginButton.addEventListener('click',function(){
+  view1.style.display = "none";
+  view2.style.display = "block";
+  view3.style.display = "none";
+});
+
+studentsButton.addEventListener('click',function(){
+  view1.style.display = "none";
+  view2.style.display = "block";
+  view3.style.display = "block";
+});
+
+
+
+/*
 var filterButton = document.getElementById('filter_button')
 var view1 = document.getElementById('view_1');
 var view2 = document.getElementById('view_2');
@@ -9,28 +29,26 @@ var id = document.getElementById("cohorts_list");
 var info1 = document.getElementById("info_1");
 
 
-enterButton.addEventListener('click',function(){
-  view1.style.display = "none";
-  view2.style.display = "block";
-  header.style.display = "block";
-  view3.style.display = "none";
-});
 
 
-platformButton.addEventListener('click',function(){
+
+
+
+
+studentsButton.addEventListener('click',function(){
   view1.style.display = "none";
   view2.style.display = "none";
   header.style.display = "block";
   view3.style.display = "block";
+  document.getElementById("cohorts_list").innerHTML = data.function1
+})
 
-  var request1 = new XMLHttpRequest();
-    request1.open('GET', 'http://127.0.0.1:8080/data/cohorts.json');
-    request1.onload = function() {
-      var cohortsData = JSON.parse(request1.responseText);
-      document.getElementById("cohorts_list").innerHTML = data.function1(cohortsData)
-    };
-    request1.send();
 
+filterButton.addEventListener('click',function(){
+  view1.style.display = "none";
+  view2.style.display = "none";
+  header.style.display = "block";
+  view3.style.display = "block";
   var request2 = new XMLHttpRequest();
     request2.open('GET', 'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json');
     request2.onload = function() {
@@ -38,15 +56,12 @@ platformButton.addEventListener('click',function(){
       document.getElementById("users_list").innerHTML = data.function2(usersData)
     };
     request2.send();
-})
 
-filterButton.addEventListener('click',function(){
-  view1.style.display = "none";
-  view2.style.display = "none";
-  header.style.display = "block";
-  view3.style.display = "block";
-  info1.style.display = "block";
-})
+})*/
+
+
+
+
 
 
 
